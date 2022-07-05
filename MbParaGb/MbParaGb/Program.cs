0bb59c77252d9f9a2bb7dec88026539354bdd61f
MrWestOFC <mister.west529@gmail.com>
2022-07-05 01:06:50 -0300

    first commit

---
+++ MbParaGb/MbParaGb/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MbParaGb {
+    
+    class Program {
+        static int mb(int mb) {
+            return mb * 1024;
+        }
+        
+        static void Main(String[] ars) {
+            int x;
+            
+            Console.Write("Digite um valor >> ");
+            x = int.Parse(Console.ReadLine());
+
+            while (x != 0) {
+                Console.WriteLine("Valor em Megabytes " + mb(x) + "Mib");
+                Console.WriteLine("Valor em Gigabytes " + x + "Gib");
+                Console.Write(">> ");
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Megabytes e GigaBytes em C#
+
+<img src="https://camo.githubusercontent.com/e2b810d3b94b5eceaf23e7126b23e5101d8d979e819d9f7f7aa09b82415d670a/68747470733a2f2f342e62702e626c6f6773706f742e636f6d2f2d6c426445432d63495437412f543975375041555f545a492f41414141414141416a31512f66494753785653644e59632f73313630302f342e676966" align="right" width="109">
+
+<hr>
+
+Programa simples em C# que mostra valores de megabytes e gigabytes quando digitado
